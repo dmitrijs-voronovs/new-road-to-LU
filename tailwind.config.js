b2b9268c0ba2cce7a9200e31539079e1c59d7a5f
dmitrijs <dmitrijs.voronovs@scandiweb.com>
2021-06-02 19:47:15 +0300

    Added main colors and JIT mdoe

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,10 @@
 module.exports = {
   purge: ["./components/**/*.js", "./pages/**/*.js"],
-  mode: 'jit',
+  mode: "jit",
   theme: {
     extend: {
       colors: {
-        p1: '#6C4897',
+        p1: "#6C4897",
         p2: "#B85093",
         p3: "#EE6681",
         p4: "#FF8F6A",
